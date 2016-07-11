c11f2afb8481f1e188210552cfed71115afb548b
ouru2006 <ouru2006@gmail.com>
2016-07-10 20:21:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask,request, render_template
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
 app = Flask(__name__)
 # TODO(ericdand): Use a user other than root.
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'crazylove'
 app.config['MYSQL_DATABASE_DB'] = 'Saiddit'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 # TODO(ericdand): Uncomment the line below once
 # you have the database actually set up.
-# mysql.init_app(app)
+mysql.init_app(app)
 
-@app.route('/')
-def main():
-    return render_template('index.html')
+@app.route('/', methods=['GET'])
+def signin_form():
+	return render_template('loginpage.html')
+	
+@app.route('/signin', methods=['POST'])
+def signin():
+	username = request.form['username']
+	password = request.form['password']
+	cursor=mysql.connect().cursor()
+	cursor.execute("SELECT * from Accounts where username='"+ username +"'AND password='"+ password+"';" )
+	data = cursor.fetchone()
+	if data is None:
+		return render_template('loginpage.html', message='Incorrect username or password', username=username)
+	else:
+		return render_template('index.html', username=username)
+	
 
 if __name__ == '__main__':
     app.run()
