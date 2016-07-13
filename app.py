6c809b134414dcd8c029bef76252917eceb02545
Eric Dand <eric.c.dand@gmail.com>
2016-07-12 19:54:10 -0700

    Add a stub user, display the users id on the frontpage.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-from flask_mysql import MySQL
-from flask_login import LoginManager
+from flaskext.mysql import MySQL
+from flask_login import LoginManager, login_user
 
 from user import SaidditUser
 
@@ -15,6 +15,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 # you have the database actually set up.
 # mysql.init_app(app)
 
+app.secret_key = '\x88j\xd7\x1f&\xc6\x87(X\xa0\xc9\xc4\x96\xffL\xbe\xc8K\xa5JM\xdc\xae+'
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -24,6 +25,8 @@ def load_user(user_id):
 
 @app.route('/')
 def main():
+    # TODO(edand): This is just a demo user.
+    login_user(SaidditUser('admin'))
     return render_template('index.html')
 
 if __name__ == '__main__':
