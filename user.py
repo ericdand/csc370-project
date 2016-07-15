bcde19356b7143747be9b99a3c8967ae32bc50f3
Leanne Feng <leanne_feng@yahoo.com>
2016-07-15 15:23:07 -0700

    fix login bug

--- user.py
+++ user.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 class SaidditUser(UserMixin):
     def __init__(self, id):
         self.id = id
+        self.subscriptions = []
 
     def get_id(self):
         return unicode(self.id)
